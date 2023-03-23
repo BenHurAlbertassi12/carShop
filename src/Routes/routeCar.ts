b834abf37188ab2fb5e955cc0b0521f668bf6f44
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-03-23 12:17:38 -0300

    Criando as rotas iniciais "/cars"

---
+++ src/Routes/routeCar.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const routeCar = Router();
+
+routeCar.get('/');
+
+export default routeCar;
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import carRouter from './Routes/routeCar';
 
 const app = express();
+
+app.use('/cars', carRouter);
 
 export default app;

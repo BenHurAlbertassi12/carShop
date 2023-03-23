134cbd715344dae42abf6e514da15a689169cc82
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-03-23 12:50:18 -0300

    Criando a rota /motorcycles

---
+++ src/Routes/routeMotorcycles.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const motocycleRouter = Router();
+
+motocycleRouter.get('/');
+  
+export default motocycleRouter;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import carRouter from './Routes/routeCar';
+import motocycleRouter from './Routes/routeMotorcycles';
 
 const app = express();
 
 app.use('/cars', carRouter);
+app.use('/motorcycles', motocycleRouter);
 
 export default app;
